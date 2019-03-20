73f5eadb80ce7b40a41706adc60461ad416b0304
SwainWong <swain@SwainWongdeMacBook-Pro-2.local>
2019-03-20 15:51:09 +0800

    first block

---
+++ index.js
@@ -0,0 +1,19 @@
+let {Blockchain} = require('./chain');
+let {Block} = require('./block');
+
+let SwainCoin = new Blockchain();
+
+console.log(SwainCoin)
+let counter = 0;
+// 添加第一个区块     
+SwainCoin.addBlock(new Block(counter++,(new Date()).getDate(),{data:'the'+counter+'block'}));
+
+// 添加定时添加区块   
+setInterval(_=>{
+    SwainCoin.addBlock(new Block(counter++,(new Date()).getDate(),{data:'the'+counter+'block'}));
+},3000);
+
+
+
+// 
+  
